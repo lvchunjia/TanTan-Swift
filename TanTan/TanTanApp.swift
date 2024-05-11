599be300985c234dedb4bf4aeac01860110e5e4d
lvchunjia <879021430@qq.com>
2024-05-11 14:54:44 +0800

    Initial Commit

---
+++ TanTan/TanTanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanTanApp.swift
+//  TanTan
+//
+//  Created by 中律十七 on 2024/5/11.
+//
+
+import SwiftUI
+
+@main
+struct TanTanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
