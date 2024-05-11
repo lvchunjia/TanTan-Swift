daa8d8bebaa8fab4358d7b156c54cb450a0532e3
lvchunjia <879021430@qq.com>
2024-05-11 15:10:57 +0800

    feat: custom tab bar v1

--- TanTan/ContentView.swift
+++ TanTan/ContentView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    var tabs: [TabBarItem] = [
+        TabBarItem(title: "TanTan",icon: "rectangle.fill.on.rectangle.fill"),
+        TabBarItem(title: "Live",icon: "play.tv.fill"),
+        TabBarItem(title: "Message",icon: "message.fill"),
+        TabBarItem(title: "Person",icon: "person.fill"),
+    ]
+    
+    @State var selectedTab: TabBarItem = TabBarItem(title: "TanTan",icon: "rectangle.fill.on.rectangle.fill")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            HStack {
+                ForEach (tabs, id: \.self) { tab in
+                    createTabBarItem(tabBarItem: tab)
+                        .onTapGesture {
+                            selectedTab = tab
+                        }
+                }
+            }
         }
-        .padding()
+        
     }
+    
+    func createTabBarItem(tabBarItem: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tabBarItem.icon)
+                .font(.system(size: 24, weight: .bold))
+                .foregroundColor(selectedTab == tabBarItem ? .accentColor : Color.gray.opacity(0.5))
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            Text(tabBarItem.title)
+                .font(.system(size: 10))
+                .foregroundColor(selectedTab == tabBarItem ? .accentColor : Color.gray.opacity(0.5))
+        }
+    }
+}
+
+struct TabBarItem: Hashable {
+    let title: String
+    let icon: String
 }
 
 #Preview {
